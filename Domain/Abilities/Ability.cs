3d564a50f8aea3d5bcb4a685645c7de140d1d7b5
422UR4H <tiagosindra@id.uff.br>
2024-04-02 20:01:39 -0300

    Domain: add abilities

---
+++ Domain/Abilities/Ability.cs
@@ -0,0 +1,13 @@
+using HxH_RPG_Environment.Domain.Experiences;
+
+namespace HxH_RPG_Environment.Domain.Abilities;
+
+public class Ability(Experience exp) : ICascadeUpgrade
+{
+  public Experience Exp { get; } = exp;
+
+  public void Upgrade(int value)
+  {
+    Exp.IncreasePoints(value);
+  }
+}
