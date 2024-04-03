42dbd398e3357e5ea7d379d4c28a945e9174a473
422UR4H <tiagosindra@id.uff.br>
2024-04-03 01:28:50 -0300

    Domain: add enums

---
+++ Domain/Enums/AttributeName.cs
@@ -0,0 +1,24 @@
+namespace HxH_RPG_Environment.Domain.Enums;
+
+public enum AttributeName
+{
+  // Physicals
+  Constitution,
+  Defense,
+  Strength,
+  Velocity,
+  Agility,
+  ActionSpeed,
+  Dexterity,
+  Reflex,
+  Sense,
+
+  // Mentals
+  Resilience,
+  Adaptability,
+  Weighting,
+  Creativity,
+
+  // Spirituals
+  Spirit
+}
