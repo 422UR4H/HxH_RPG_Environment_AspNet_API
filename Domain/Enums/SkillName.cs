971410ed20276b0a6feb4fcd7d38f91e1d1e35b8
422UR4H <tiagosindra@id.uff.br>
2024-04-04 16:02:04 -0300

    Domain: add SkillName

---
+++ Domain/Enums/SkillName.cs
@@ -0,0 +1,65 @@
+namespace HxH_RPG_Environment.Domain.Enums;
+
+public enum SkillName
+{
+  // PHYSICALS
+  // Constitution
+  Vitality,
+  Resistance,
+  Breath,
+  Heal,
+
+  // Defense
+  Defense,
+
+  // Strength
+  Climb,
+  Push,
+  Pull,
+  Grab,
+
+  // Velocity
+  Run,
+  Swim,
+  Jump,
+
+  // Agility
+  Dodge,
+  Accelerate,
+  Brake,
+
+  // Action Speed
+  ActionSpeed,
+  Feint,
+
+  // Flexibility
+  Acrobatics,
+  Sneak,
+
+  // Dexterity?
+  Reflex,
+  Accuracy,
+  Stealth,
+  SleightOfHand,
+
+  // Sense
+  Vision,
+  Hearing,
+  Smell,
+  Tact,
+  Taste,
+  Balance,
+
+  // Instinct
+  Intuition,
+
+
+  // MENTALS
+  // Resilience
+  // Adaptability
+  // Weighting
+  // Creativity
+
+  // Spirituals
+  Spirit
+}
