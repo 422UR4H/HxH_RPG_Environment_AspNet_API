90197f39343e7ccee90cb6632b52411a4c00d366
422UR4H <tiagosindra@id.uff.br>
2024-04-04 16:00:51 -0300

    Domain: add SkillName

--- Domain/Skills/ISkill.cs
+++ Domain/Skills/ISkill.cs
@@ -2,23 +2,8 @@ using HxH_RPG_Environment.Domain.Experiences;
 
 namespace HxH_RPG_Environment.Domain.Skills;
 
-public interface ISkill : ICascadeUpgrade
+public interface ISkill : IIncreaseCascadeExp
 {
   public ICascadeUpgrade AttributeExp { get; }
-
-  public new void Upgrade(int exp)
-  {
-    Exp.IncreasePoints(exp);
-    AttributeExp.Upgrade(exp);
-  }
-
-  public int GetExp()
-  {
-    return Exp.Points;
-  }
-
-  public int GetLvl()
-  {
-    return Exp.GetLvl();
-  }
+  public Experience AbilitySkillsExp { get; }
 }
