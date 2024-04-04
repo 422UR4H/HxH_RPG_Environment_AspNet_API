3d473fec08c53bf35768a7bea2c0dc83f261b190
422UR4H <tiagosindra@id.uff.br>
2024-04-04 17:32:01 -0300

    Domain: fix interface and its method renaming to semanthic better name

--- Domain/Experiences/IIncreaseCascadeExp.cs
+++ Domain/Experiences/IIncreaseCascadeExp.cs
@@ -1,7 +1,7 @@
 namespace HxH_RPG_Environment.Domain.Experiences;
 
-public interface IIncreaseCascadeExp
+public interface ITriggerCascadeExp
 {
   public Experience Exp { get; }
-  public void IncreaseCascadeExp(int exp);
+  public void TriggerCascadeUpgrade(int exp);
 }
