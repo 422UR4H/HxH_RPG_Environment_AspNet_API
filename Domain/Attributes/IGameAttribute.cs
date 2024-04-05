70c1ebb630f601982cb3557524e5fec22902a5c0
422UR4H <tiagosindra@id.uff.br>
2024-04-05 15:39:28 -0300

    Domain: refactor cleaning dead code

--- Domain/Attributes/IGameAttribute.cs
+++ Domain/Attributes/IGameAttribute.cs
@@ -7,18 +7,8 @@ public interface IGameAttribute : ICascadeUpgrade
   public int Points { get; }
   public ICascadeUpgrade AbilityExp { get; }
 
-  public int GetExp()
-  {
-    return Exp.Points;
-  }
-
-  public int GetLvl()
-  {
-    return Exp.GetLvl();
-  }
-
   public int GetPower()
   {
-    return Points + GetLvl();
+    return Points + Exp.GetLvl();
   }
 }
