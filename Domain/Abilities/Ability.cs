01a464c5c140f0f562c9f18f1aa3e810553608ec
422UR4H <tiagosindra@id.uff.br>
2024-04-05 01:34:38 -0300

    Domain: fix Ability to respect SOLID principles

--- Domain/Abilities/Ability.cs
+++ Domain/Abilities/Ability.cs
@@ -2,12 +2,14 @@ using HxH_RPG_Environment.Domain.Experiences;
 
 namespace HxH_RPG_Environment.Domain.Abilities;
 
-public class Ability(Experience exp) : ICascadeUpgrade
+public class Ability(Experience exp, IEndCascadeUpgrade characterExp) : ICascadeUpgrade
 {
   public Experience Exp { get; } = exp;
+  public IEndCascadeUpgrade CharacterExp { get; } = characterExp;
 
-  public void Upgrade(int exp)
+  public void CascadeUpgrade(int exp)
   {
     Exp.IncreasePoints(exp);
+    CharacterExp.TriggerEndUpgrade(exp);
   }
 }
