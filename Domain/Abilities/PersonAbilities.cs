0f822e633aee8c6c5330615e476fc66d57ad362d
422UR4H <tiagosindra@id.uff.br>
2024-04-06 14:47:13 -0300

    Domain: add Talent

--- Domain/Abilities/PersonAbilities.cs
+++ Domain/Abilities/PersonAbilities.cs
@@ -5,16 +5,22 @@ namespace HxH_RPG_Environment.Domain.Abilities;
 
 public class PersonAbilities
 {
+  private const double TALENT_COEFFICIENT = 10.0;
   private const double PHYSICAL_COEFFICIENT = 20.0;
   private const double MENTAL_COEFFICIENT = 15.0;
   private const double SPIRITUAL_COEFFICIENT = 5.0;
   private const double SKILLS_COEFFICIENT = 5.0;
+
+  public Talent Talent { get; }
   private readonly Dictionary<AbilityName, Ability> abilities = [];
   IEndCascadeUpgrade CharacterExp { get; }
 
   public PersonAbilities(IEndCascadeUpgrade characterExp)
   {
     CharacterExp = characterExp;
+
+    Experience talentExp = new(new ExpTable(TALENT_COEFFICIENT));
+    Talent = new(talentExp);
 
     Experience physicalExp = new(new ExpTable(PHYSICAL_COEFFICIENT));
     abilities.Add(AbilityName.PHYSICALS, new(physicalExp, CharacterExp));
@@ -29,8 +35,6 @@ public class PersonAbilities
     abilities.Add(AbilityName.SKILLS, new(skillsExp, CharacterExp));
 
     // knowlegde exp
-
-    // talent
   }
 
   // TODO: refactor to throw exception
---
+++ Domain/Abilities/Talent.cs
@@ -0,0 +1,8 @@
+using HxH_RPG_Environment.Domain.Experiences;
+
+namespace HxH_RPG_Environment.Domain.Abilities;
+
+public class Talent(Experience exp)
+{
+  public Experience Exp = exp;
+}
