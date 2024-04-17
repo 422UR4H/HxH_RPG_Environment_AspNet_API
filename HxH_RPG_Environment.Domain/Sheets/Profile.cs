b71b19e303baa35bcc692ebc1540c366d6f3df51
422UR4H <tiagosindra@id.uff.br>
2024-04-17 02:21:45 -0300

    WIP: fix optional fields

--- HxH_RPG_Environment.Domain/Sheets/Profile.cs
+++ HxH_RPG_Environment.Domain/Sheets/Profile.cs
@@ -3,13 +3,12 @@ namespace HxH_RPG_Environment.Domain.Sheets;
 public class Profile(
   string nickname,
   string fullname,
-  string description,
-  string briefDescription,
+  string? description,
   DateOnly birthday)
 {
   public string Nickname { get; set; } = nickname;
   public string Fullname { get; set; } = fullname;
-  public string Description { get; set; } = description;
-  public string BriefDescription { get; set; } = briefDescription;
+  public string? Description { get; set; } = description;
+  public string? BriefDescription { get; set; }
   public DateOnly Birthday { get; } = birthday;
 }
